2dbce8f78578fa0664e88966e5b79fb46a0593ad
Tejas Shah <tejasshah2k19@gmail.com>
2024-04-24 14:37:53 +0530

    dao insert' .

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bean.UserBean;
+import com.dao.UserDao;
 
 import jakarta.validation.Valid;
 
 @Controller
 public class SessionController {
+
+	@Autowired
+	UserDao userDao;
 
 	@GetMapping("signup")
 	public String signup(Model model) {
@@ -23,11 +29,12 @@ public class SessionController {
 	@PostMapping("saveuser")
 	public String saveUser(@ModelAttribute("user") @Valid UserBean user, BindingResult result, Model model) {
 
-		model.addAttribute("user", user);//data + error 
-		
+		model.addAttribute("user", user);// data + error
+
 		if (result.hasErrors() == true) {
 			return "Signup";
 		} else {
+			userDao.addUser(user);
 			return "Home";
 		}
 	}
